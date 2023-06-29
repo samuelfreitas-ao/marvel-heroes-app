e1bfc2098c2872aa3b58ffa7363e6692510c2f16
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 09:22:25 +0100

    refactor: Config app´entry point

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Home } from './src/presentation/screens'
+import { StatusBar } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+			<Home />
+		</SafeAreaProvider>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
