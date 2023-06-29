219ffc005cf5020489bc501661cd01fd8d6caa8e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 19:53:45 +0100

    style: Fix View display

--- src/presentation/components/character-item/styled.ts
+++ src/presentation/components/character-item/styled.ts
@@ -1,30 +1,31 @@
 import { Dimensions } from 'react-native'
 import { styled } from 'styled-components/native'
 
-const { width: screenWidth } = Dimensions.get('screen')
+const { width } = Dimensions.get('screen')
 
-const itemHeight = screenWidth - 32
+const itemHeight = width - 32
+const itemWidth = itemHeight
 
 export const Container = styled.View`
 	position: relative;
 	display: flex;
-	flex: 1;
 	margin-bottom: 16px;
 	height: ${itemHeight}px;
 `
 export const ImageContainer = styled.View`
 	justify-content: center;
 	align-items: center;
+	height: ${itemHeight}px;
 `
 export const ItemImage = styled.Image<{ unavailable?: string }>`
 	opacity: ${(props) => (props?.unavailable ? 0.6 : 0.8)};
-	width: 100%;
+	width: ${itemWidth}px;
 	height: ${itemHeight}px;
 `
 export const ItemTitle = styled.View`
 	background-color: rgba(90, 126, 118, 0.8);
 	position: absolute;
 	bottom: 0;
-	width: 100%;
+	width: ${itemWidth}px;
 	padding: 4px;
 `
