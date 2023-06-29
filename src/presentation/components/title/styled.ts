1d0c3fd608590fc57edf6e32af578b8a4d60c7ab
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 11:59:09 +0100

    style: Title presentation

--- src/presentation/components/title/styled.ts
+++ src/presentation/components/title/styled.ts
@@ -1,6 +1,10 @@
 import { styled } from 'styled-components/native'
 
 export const Container = styled.View`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
 	background-color: #363838;
 	padding: 8px;
 	font-weight: 500;
