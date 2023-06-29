9b0014364d0410ca00b5bc21d17ad2365f6444db
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 10:43:02 +0100

    refactor: Text component

--- src/presentation/components/text/index.tsx
+++ src/presentation/components/text/index.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react'
+import { TextProps } from 'react-native'
 import { Container } from './styled'
 
-type TextProps = {
+type TextProp = TextProps & {
 	children?: ReactNode
 	text?: ReactNode
+	color?: string
 }
 
-export function Text({ children, text }: TextProps) {
-	return <Container>{children ?? text}</Container>
+export function Text({ children, text, color, ...props }: TextProp) {
+	return (
+		<Container color={color} {...props}>
+			{children ?? text}
+		</Container>
+	)
 }
--- src/presentation/components/text/styled.ts
+++ src/presentation/components/text/styled.ts
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components/native'
 
-export const Container = styled.Text`
+export const Container = styled.Text<{ color?: string }>`
 	font-size: 16px;
+	color: ${(props) => props?.color ?? '#666'};
 `
