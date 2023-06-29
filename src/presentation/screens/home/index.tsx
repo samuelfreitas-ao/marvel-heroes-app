b6ff3d832a4857a2b8630274e0521dcd67621cad
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 10:44:41 +0100

    refactor: Add header and title on home screen

--- src/presentation/screens/home/index.tsx
+++ src/presentation/screens/home/index.tsx
@@ -1,10 +1,16 @@
-import { Text } from '../../components'
-import { Layout, LayoutBody } from '../../components'
+import { Header, Layout, LayoutBody, Text, Title } from '../../components'
+import { SearchBar } from '../../components'
 
 export function Home() {
 	return (
 		<Layout>
+			<Header>
+				<SearchBar />
+			</Header>
 			<LayoutBody>
+				<Title>
+					<Text text="Personagens" color="white" />
+				</Title>
 				<Text text="Olá" />
 			</LayoutBody>
 		</Layout>
