3c8a8094f37736d48b139e5113e2ebf094282569
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 11:58:37 +0100

    feat: Add IconSearch component

--- src/presentation/components/icons.tsx
+++ src/presentation/components/icons.tsx
@@ -1,14 +1,15 @@
-import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons, AntDesign } from '@expo/vector-icons'
 import { GestureResponderEvent } from 'react-native'
 
-type IconProps = {
+export type IconProps = {
 	color?: string
 	size?: number
 	style?: object
 	onPress?: (event: GestureResponderEvent) => void
 }
 
-export const IconSearch = (props: IconProps) => <MaterialIcons name="search" {...props} />
 export const IconBack = (props: IconProps) => (
 	<MaterialIcons name="arrow-back" {...props} />
 )
+export const IconSearch = (props: IconProps) => <MaterialIcons name="search" {...props} />
+export const IconSpinner = (props: IconProps) => <AntDesign name="loading1" {...props} />
