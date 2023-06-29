57c7712b2459176943cca2946fdb0f03bf3d806d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-29 09:21:53 +0100

    feat: Add home screen

---
+++ src/presentation/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from '../../components'
+import { Layout, LayoutBody } from '../../components'
+
+export function Home() {
+	return (
+		<Layout>
+			<LayoutBody>
+				<Text text="Olá" />
+			</LayoutBody>
+		</Layout>
+	)
+}
