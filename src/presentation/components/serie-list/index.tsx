928ed042db84c116795577271bd3d22c2f0c42ab
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-30 17:18:29 +0100

    feat: Add SerieList component

---
+++ src/presentation/components/serie-list/index.tsx
@@ -0,0 +1,25 @@
+import { Serie } from '../../../domain/models'
+import { Text } from '../text'
+import { Container, Item, List, Title } from './styled'
+
+type SerieProps = {
+	series: Serie[]
+}
+
+export function SerieList({ series }: SerieProps) {
+	return (
+		<Container>
+			<Title>
+				<Text text="Participação em séries" />
+				<Text text={`(${series.length})`} />
+			</Title>
+			<List>
+				{series.map((serie) => (
+					<Item key={serie.name}>
+						<Text text={`- ${serie.name}`} />
+					</Item>
+				))}
+			</List>
+		</Container>
+	)
+}
---
+++ src/presentation/components/serie-list/styled.ts
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components/native'
+
+export const Container = styled.View`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+`
+export const Title = styled.View`
+	display: flex;
+	align-items: center;
+	gap: 4px;
+`
+export const List = styled.View`
+	display: flex;
+	flex-direction: column;
+	gap: 4px;
+	padding: 0;
+`
+export const Item = styled.View`
+	display: flex;
+	flex-direction: column;
+	font-size: 12px;
+`
