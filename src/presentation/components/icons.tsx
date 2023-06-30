76d47dbbf44ac1a4e28678e6679c1e05c37c2655
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-30 23:10:14 +0100

    feat: Add social media icons

--- src/presentation/components/icons.tsx
+++ src/presentation/components/icons.tsx
@@ -1,4 +1,12 @@
-import { MaterialIcons, AntDesign, Ionicons } from '@expo/vector-icons'
+import {
+	MaterialIcons,
+	AntDesign,
+	Ionicons,
+	Entypo,
+	FontAwesome5,
+	FontAwesome,
+	Fontisto
+} from '@expo/vector-icons'
 import { GestureResponderEvent } from 'react-native'
 
 export type IconProps = {
@@ -11,7 +19,22 @@ export type IconProps = {
 export const IconBack = (props: IconProps) => (
 	<MaterialIcons name="arrow-back" {...props} />
 )
+export const IconInfo = (props: IconProps) => (
+	<Entypo name="info-with-circle" {...props} />
+)
 export const IconMenu = (props: IconProps) => <Ionicons name="md-menu" {...props} />
 export const IconSearch = (props: IconProps) => <MaterialIcons name="search" {...props} />
 export const IconSend = (props: IconProps) => <Ionicons name="send-sharp" {...props} />
 export const IconSpinner = (props: IconProps) => <AntDesign name="loading1" {...props} />
+
+export const IconGithub = (props: IconProps) => <AntDesign name="github" {...props} />
+export const IconLinkedIn = (props: IconProps) => (
+	<AntDesign name="linkedin-square" {...props} />
+)
+export const IconFacebook = (props: IconProps) => (
+	<FontAwesome5 name="facebook" {...props} />
+)
+export const IconWhatsApp = (props: IconProps) => (
+	<FontAwesome name="whatsapp" {...props} />
+)
+export const IconSite = (props: IconProps) => <Fontisto name="world" {...props} />
