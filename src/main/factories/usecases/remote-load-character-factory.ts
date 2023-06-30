4250f2472c53eccfacfef70ef1ee117485f59bf3
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-30 19:36:47 +0100

    refactor: Rename factory

--- src/main/factories/usecases/remote-load-character-factory.ts
+++ src/main/factories/usecases/remote-load-character-factory.ts
@@ -2,6 +2,6 @@ import { RemoteLoadCharacters } from '../../../data/usecases'
 import { AxiosHttpClient } from '../../../infra/http'
 import { makeApiUrl } from '../http'
 
-export const makeRemoteLoadHeroes = () => {
+export const makeRemoteLoadCharacter = () => {
 	return new RemoteLoadCharacters(makeApiUrl('/characters'), new AxiosHttpClient())
 }
