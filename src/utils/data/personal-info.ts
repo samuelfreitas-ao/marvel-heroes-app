41802a70c2817c1c106763b61c10170b72f723e2
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-30 23:10:58 +0100

    feat: Add personal data util

---
+++ src/utils/data/personal-info.ts
@@ -0,0 +1,10 @@
+export function getPersonalInfo() {
+	return {
+		name: 'Samuel Freitas',
+		site: 'https://samuelfreitas.vercel.app',
+		linkedin: 'https://linkedin.com/in/samuelfreitas-ao',
+		github: 'https://github.com/samuelfreitas-ao',
+		whatsAppLink: 'https://wa.me/message/BEZBIUEUNXEZH1',
+		facebook: 'https://facebook.com/samuelfreitas.ao'
+	}
+}
