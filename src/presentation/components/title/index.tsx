c387e8b2751e06edb89f484617ca6ca82e0c6730
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-30 13:17:00 +0100

    feat: Add button on title component to navigate on screens

--- src/presentation/components/title/index.tsx
+++ src/presentation/components/title/index.tsx
@@ -4,24 +4,28 @@ import { Button, Container } from './styled'
 import { View } from 'react-native'
 import { IconBack } from '../icons'
 import { Text } from '../text'
+import { IScreen } from '../../../@types'
+import { useNavigation } from '@react-navigation/native'
 
-type TitleProps = { children: ReactNode; backTo?: string }
+type TitleProps = {
+	children: ReactNode
+	backTo?: keyof IScreen
+}
 
 export function Title({ children, backTo }: TitleProps) {
+	const { navigate } = useNavigation()
 	const handleBackHome = useCallback(() => {
-		console.log('Backing')
+		navigate(backTo ?? 'home')
 	}, [])
 
 	return (
 		<>
 			<Container>{children}</Container>
 			{backTo && (
-				<View>
-					<Button onPress={handleBackHome}>
-						<IconBack />
-						<Text text="Home" />
-					</Button>
-				</View>
+				<Button onPress={handleBackHome}>
+					<IconBack size={16} color="#fff" />
+					<Text text="Home" color="#fff" />
+				</Button>
 			)}
 		</>
 	)
